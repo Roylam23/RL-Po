63d8b89e48b589a02e0c0a2b8e9650230bba0b80
Roylam23 <roylam23@gmail.com>
2021-09-11 02:28:06 +0800

    Create Home Page

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Home from './Page/Home'
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ src/Page/Home.jsx
@@ -0,0 +1,15 @@
+import Nav from "./Component/Nav";
+import Code from './Component/Code'
+
+
+const Home = () => {
+
+  return (
+    <>
+      <Nav />
+      <Code />
+    </>
+  );
+};
+
+export default Home;
